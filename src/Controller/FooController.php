b46b1535c9213a53ccb05434fc7757fdf10acfea
Michal Repec <mikerepec@gmail.com>
2019-01-24 14:38:05 +0100

    Initial commit

---
+++ src/Controller/FooController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FooController extends AbstractController
+{
+    /**
+     * @Route("/foo", name="foo")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/FooController.php',
+        ]);
+    }
+}
